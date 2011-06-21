8f420f2d0db51831bfb9416fd8f17299579fe37c
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-06-21 15:45:06 +0000

    Add the check_writable nagios plugin to ensure drives are writable.

---
+++ recipes/check_writable.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: opsview
+# Recipe:: check_writable
+#
+# Copyright 2011, NREL
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "perl::data_random"
+
+nrpe_plugin "check_writable"
