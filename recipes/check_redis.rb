b1509a79e2dacb254148b72c1816aa12cac82d29
Nick Muerdter <nick.muerdter@nrel.gov>
2013-03-10 15:25:03 -0600

    Updates for new rbenv cookbook.

--- recipes/check_redis.rb
+++ recipes/check_redis.rb
@@ -7,19 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "rbenv::global_version"
+include_recipe "rbenv::system"
 
-rbenv_gem "redis" do
-  ruby_version node[:rbenv][:install_global_version]
-end
+rbenv_gem "redis"
 
 if(node[:languages][:ruby][:version].to_f < 1.9)
-  rbenv_gem "SystemTimer" do
-    ruby_version node[:rbenv][:install_global_version]
-  end
+  rbenv_gem "SystemTimer"
 end
 
 nrpe_plugin "check_redis" do
   source "nagios_plugins/nagios-check-redis/check_redis"
-  env "env PATH=#{node[:rbenv][:install_prefix]}/rbenv/shims:#{node[:rbenv][:install_prefix]}/rbenv/bin:$PATH "
+  env "env PATH=#{node[:rbenv][:root_path]}/shims:#{node[:rbenv][:root_path]}/bin:$PATH "
 end
