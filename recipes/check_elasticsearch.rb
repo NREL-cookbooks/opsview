68263f7e33afc7a0bf849960044b35f9af6e7c5a
Nick Muerdter <nick.muerdter@nrel.gov>
2014-02-25 23:38:07 -0700

    Add check_elasticsearch script.

---
+++ recipes/check_elasticsearch.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: opsview
+# Recipe:: check_elasticsearch
+#
+# Copyright 2014, NREL
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "opsview::client"
+include_recipe "python"
+
+python_pip "nagioscheck" do
+  action :install
+end
+
+# https://github.com/anchor/nagios-plugin-elasticsearch
+nrpe_plugin "check_elasticsearch" do
+  remote_file true
+  source "https://raw.github.com/anchor/nagios-plugin-elasticsearch/176e392a97eedb1567ab1c0d05a2d4fce3aef3a3/check_elasticsearch"
+  checksum "fde70383ac5125c3e7c368651bf7ca78b6b306c6cafaf0ac0987830d52655c17"
+end
