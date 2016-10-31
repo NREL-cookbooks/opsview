05972e0de579c63d0b84f7ff0b621e5d6bb5c2e8
Nick Muerdter <nick.muerdter@nrel.gov>
2016-10-31 14:47:16 -0600

    Use HTTPS for download.opsview.com downloads.

--- recipes/yum_repo.rb
+++ recipes/yum_repo.rb
@@ -1,6 +1,6 @@
 yum_repository("opsview") do
   description "Opsview"
-  baseurl "http://downloads.opsview.com/opsview-core/latest/yum/rhel/$releasever/$basearch"
+  baseurl "https://downloads.opsview.com/opsview-core/latest/yum/rhel/$releasever/$basearch"
   enabled true
   gpgcheck false
   action :create
