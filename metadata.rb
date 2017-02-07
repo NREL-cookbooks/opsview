bb4f38114137439c303e622c68395e89c4a9f100
Nick Muerdter <nick.muerdter@nrel.gov>
2017-02-07 15:04:17 -0700

    Add script to check external website connectivity.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "nick.muerdter@nrel.gov"
 license          "All rights reserved"
 description      "Installs/Configures opsview"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.2"
+version          "0.1.3"
 
 depends "acl"
 depends "apache2"
