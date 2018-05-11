5af47160651550085721264e0642d71786c2d386
Nick Muerdter <nick.muerdter@nrel.gov>
2018-05-11 14:34:08 -0600

    Fix deprecated random password generation approach.

--- attributes/server.rb
+++ attributes/server.rb
@@ -7,18 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
-::Chef::Node.send(:include, Opscode::OpenSSL::Password)
+Chef::Recipe.send(:include, OpenSSLCookbook::RandomPassword)
 
 default[:opsview][:db][:database] = "opsview"
 default[:opsview][:db][:username] = "opsview"
-normal_unless[:opsview][:db][:password] = secure_password
+normal_unless[:opsview][:db][:password] = random_password
 
 default[:opsview][:db][:runtime][:database] = "runtime"
 default[:opsview][:db][:runtime][:username] = "nagios"
-normal_unless[:opsview][:db][:runtime][:password] = secure_password
+normal_unless[:opsview][:db][:runtime][:password] = random_password
 
 default[:opsview][:backup][:dir] = "/usr/local/nagios/var/backups"
 default[:opsview][:backup][:retention] = 30
 
-normal_unless[:opsview][:authtkt_shared_secret] = secure_password
-normal_unless[:opsview][:nrd_shared_password] = secure_password
+normal_unless[:opsview][:authtkt_shared_secret] = random_password
+normal_unless[:opsview][:nrd_shared_password] = random_password
